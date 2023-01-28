eecb55acb9a6d601efa000cfb79f00bb47b43ac5
harachaan <hr1021ry@icloud.com>
2023-01-28 17:26:08 +0900

    seed tarochan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory()->create([
+            'name' => 'tarochan',
+            'email' => 'tarochan@example.com',
+        ]);
+
         \App\Models\User::factory(10)->create();
 
+        
         // Group tableとかにダミーデータ
         $this->call([GroupsSeeder::class]);
         $this->call([ChatinputsSeeder::class]);
