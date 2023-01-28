26ca799cef918656f5c24d3e8e2c16284eda2c36
harachaan <hr1021ry@icloud.com>
2023-01-28 14:35:32 +0900

    create the function of seeder for Group table dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Group tableとかにダミーデータ
+        $this->call([GroupsSeeder::class]);
     }
 }
---
+++ database/seeders/GroupsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Str;
+use App\Models\Group;
+
+class GroupsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Group::factory()->count(3)->create();
+    }
+}
