079473fc3add6d12971da6e17a573d50b9b679a9
ST <st20027802@gmail.com>
2023-01-28 10:56:59 +0900

    chatの内容のmigrationを作成

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User_group extends Model
+class Chat extends Model
 {
     use HasFactory;
-
 }
-
-
-
--- resources/views/general/top.blade.php
+++ resources/views/general/top.blade.php
@@ -1,33 +1,44 @@
-@extends('general.layouts')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>chat</title>
+</head>
+<body>
+    <div class=".rounded-sm .border-black">
 
-@section('header')
-<header class="masthead text-center text-white">
-    <div class="masthead-content">
-        <div class="container px-5">
-            {{-- <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-            <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2> --}}
-            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+        <div class="chat-container">
+            {{-- チャット内容表示 --}}
+            @foreach ($comments as $comment)
+            <div class="chat">
+                <div class="name">
+                    <span>{{ $comment->name }}</span>
+                </div>
+                <div class="content">
+                    <div>{{ $comment->content }}</div>
+                </div>
+            </div>
+            @endforeach
+            {{-- 点数表示 --}}
+            <div class=".rounded-sm .border-black">
+                <h2>{{ $comment->score }}</h2>
+            </div>
         </div>
-    </div>
-    <div class="bg-circle-1 bg-circle"></div>
-    <div class="bg-circle-2 bg-circle"></div>
-    <div class="bg-circle-3 bg-circle"></div>
-    <div class="bg-circle-4 bg-circle"></div>
-</header>
-    
-@endsection
 
-@section('navigation')
-<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-    <div class="container px-5">
-        <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Log In</a></li>
-            </ul>
+        {{-- 入力フォーム --}}
+        <div class=".rounded-sm .border-black">
+            <div class=".rounded-sm .border-black">
+                <form action="" method="POST">
+                    @csrf
+                    <textarea name="content" placeholder="入力してください" id="" cols="30" rows="10"></textarea>
+                    <button type="submit">送信</button>
+                </form>
+            </div>
         </div>
-    </div>
-</nav>
-@endsection
+
+    </div>    
+
+</body>
+</html>
