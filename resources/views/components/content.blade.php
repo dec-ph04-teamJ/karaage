31eb01f5abb8d904e8015aa9a03e6cb9a7113c4a
ST <st20027802@gmail.com>
2023-02-01 15:33:49 +0900

    deleted extra screens

--- resources/views/components/content.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="media">
-    <div class="media-body comment-body">
-        <div class="row">
-            {{-- <span class="comment-body-user">{{$chat->name}}</span> --}}
-            <span class="comment-body-time">{{$chat->created_at}}</span>
-        </div>
-        <span class="comment-body-content">{!! nl2br(e($chat->content)) !!}</span>
-    </div>
-</div>
