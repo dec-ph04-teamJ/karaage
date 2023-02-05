d27c85b59b0cc0b01110f6b7a4d76001246db568
masaya <masaya.reon@gmail.com>
2023-02-05 18:15:30 +0900

    新しいテーブルに対してデータを保存(kanji_rate,emoji,rate,keigoテーブルにkeigoとoutput_id)

--- app/Http/Controllers/ChatInputController.php
+++ app/Http/Controllers/ChatInputController.php
@@ -45,19 +45,17 @@ class ChatInputController extends Controller
      */
     public function store(Request $request)
     {
-        
-        
         $result_input= Chatinput::create([
             'sentence' => $request->sentence,
             'user_id' => Auth::user()->id,
         ]);
-
         $input_id=Chatinput::getAllOrderByUpdated_at(Auth::user()->id)->first()->id;
+        #inputテーブルに保存。今入力した人のinput_idを取得
+
         $pythonPath =  "../app/Python/";
         $command = "python3 ".$pythonPath."test.py 2>error.log {$result_input->sentence}";
-        // コマンドを実行
         exec($command, $outputs, $return);
-
+        #コマンドを実行
 
         $keigo_lis=[];
         $outputs_keigo=explode("'",$outputs[1]);
@@ -71,11 +69,25 @@ class ChatInputController extends Controller
         #配列でpythonから渡される場合pythonで使われていた[]や""も文字列に含まれるのでそれを削除する。
         #また重複している敬語を削除したものがkeigo_lisに格納されている。
 
+
         $result_output= Chatoutput::create([
             'input_id' => $input_id,
             'user_id' => Auth::user()->id,
             "score" => (float) $outputs[0],
+            "kanji_rate"=>$outputs[2],
+            "emoji_rate"=>$outputs[4],
         ]);
+        #outputテーブルに保存。今入力した人のoutput_idを取得
+
+
+        $output_id=Chatoutput::getAllOrderByUpdated_at(Auth::user()->id)->first()->id;
+        foreach($keigo in $keigo_lis){
+            $result_keigo=Keigo::create([
+                "output_id"=>$output_id,
+                "keigo"=>$keigo,
+            ])
+        }
+        #敬語テーブルに保存。一文に対して複数ある可能性があるのでfor文で回す。
 
         return view('chatoutput.show', compact('result_input', 'result_output'));
     }
