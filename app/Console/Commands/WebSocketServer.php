28a2859adc5ba1e8bba9c3c6c721e104f7f288c8
masaya <masaya.reon@gmail.com>
2023-02-25 18:47:50 +0900

    動くか分からないけどstep5まで終わった

--- app/Console/Commands/WebSocketServer.php
+++ app/Console/Commands/WebSocketServer.php
@@ -1,59 +1,53 @@
 <?php
 
-// namespace App\Console\Commands;
+namespace App\Console\Commands;
 
-// //use Illuminate\Console\Command;
-// use Ratchet\Server\IoServer;
-// use Ratchet\Http\HttpServer;
-// use Ratchet\WebSocket\WsServer;
-// use React\EventLoop\Factory;
-// use App\Http\Controllers\SocketController;
-// // class WebSocketServer extends Command
-// // {
-// //     /**
-// //      * The name and signature of the console command.
-// //      *
-// //      * @var string
-// //      */
-// //     protected $signature = 'websocket:init';
+use Illuminate\Console\Command;
 
-// //     /**
-// //      * The console command description.
-// //      *
-// //      * @var string
-// //      */
-// //     protected $description = 'Command description';
+use Ratchet\Server\IoServer;
 
-// //     /**
-// //      * Execute the console command.
-// //      *
-// //      * @return int
-// //      */
-// //     public function handle()
-// //     {
-// //          $server = IoServer::factory(
-// //             new HttpServer(
-// //                 new WsServer(
-// //                     new SocketController()
-// //                 )
-// //             ),
-// //             8090
-// //         );
+use Ratchet\Http\HttpServer;
 
-// //         $server->run();
-// //         //return Command::SUCCESS;
-// //     }
-// // }
+use Ratchet\WebSocket\WsServer;
 
-//     require dirname(__DIR__) . '/../../vendor/autoload.php';
+use React\EventLoop\Factory;
 
-//     $server = IoServer::factory(
-//         new HttpServer(
-//             new WsServer(
-//                 new SocketController()
-//             )
-//          ),
-//         8090
-//     );
+use App\Http\Controllers\SocketController;
 
-//     $server->run();
+class WebSocketServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'websocket:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //return 0;
+
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new SocketController()
+                )
+            ),
+            8090
+        );
+
+        $server->run();
+    }
+}
