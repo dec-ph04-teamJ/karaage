ec236cac3ebdf35b20633519fc024266a6a1c8bc
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2023-02-27 09:14:30 +0900

    remove old dashboard route

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 
 
